a963cd0e9dcd6158799ed7fe6a25da736ec59fa1
pro100user <bt769271@gmail.com>
2023-07-12 22:53:10 +0300

    feat: add tests for task 1

--- task1.rb
+++ task1.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module Validation
   MAX_NAME_LENGTH = 40
-  NAME_REGEX = /\A[A-Za-z\-]+\z/
+  NAME_REGEX = /\A(?:[\p{Lu}\s-]*\p{Lu}[\p{L}\s-]*|[\p{Lu}])\z/
   INN_REGEX = /\A[A-Z]{2}\d{10}\z/
 
   def self.valid_name?(string)
@@ -20,12 +20,37 @@ end
 
 puts "\n========== Task 1: ==========\n\n"
 
+# test
+# valid names
+puts "start test names (expected 'true')"
+puts Validation.valid_name?("Anna")  # true
+puts Validation.valid_name?("Anna-Maria")  # true
+puts Validation.valid_name?("Каріна-Ірина")  # true
+puts "end test\n\n"
+
+# invalid names
+puts "start test names (expected 'false')"
+puts Validation.valid_name?("An-")  # false
+puts Validation.valid_name?("-")  # false
+puts Validation.valid_name?("aN")  # false
+puts Validation.valid_name?("AnnA")  # false
+puts "end test\n\n"
+
 puts Validation.valid_name?("Maria-Anna")  # true
 puts Validation.valid_name?("Bogdan Tkachuk")  # false (contains a space)
 puts Validation.valid_name?("NotValidName1")  # false (contains a number)
 puts Validation.valid_name?("SoLongNameExpectToReturnFalseBecauseNameHasMoreFourtyLetters")  # false (too long)
 
 puts "\n========== Task 2: ==========\n\n"
+
+#test
+# Invalid INNs
+puts "start test INNs (expected 'false')"
+puts Validation.valid_inn?("AЯ6787787654")  # false
+puts Validation.valid_inn?("az1234567898")  # false
+puts Validation.valid_inn?("5676567656AZ")  # false
+puts Validation.valid_inn?("AZ78987")  # false
+puts "end test\n\n"
 
 puts Validation.valid_inn?("AZ1234543456")  # true
 puts Validation.valid_inn?("ABCD12345678")  # false (invalid format)
